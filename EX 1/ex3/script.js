262583bdc539da3431a39df60d15ad783ed128f1
Jp9902 <jplpd013@gmail.com>
2024-10-28 08:31:28 -0300

    números arredondados

---
+++ EX 1/ex3/script.js
@@ -0,0 +1,9 @@
+let numeros = [1.2, 3.7, 4.5, 6.1, 8.9];
+ 
+let Numerosarrendondados = []
+ 
+numeros.forEach(function (Numerosarr){
+    Numerosarrendondados = (Math.ceil (Numerosarr))
+    console.log(Numerosarrendondados)
+})
+ 
